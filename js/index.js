a9c47af27ddfbe8e603b5297e665fa825d4156b5
dimajun <drankendmitrij@gmail.com>
2024-06-02 14:41:32 +0300

    Added modal window

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const modal = document.querySelector("#modal");
+    const openBtn = document.querySelector('#openModalBtn');
+    const closeBtn = document.querySelector('#closeModalBtn');
+
+    openBtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+        document.documentElement.style.overflow = 'hidden';
+    });
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+        document.documentElement.style.overflow = 'auto';
+    });
+
+    window.addEventListener('click', (e) => {
+        if(e.target == modal) {
+            modal.style.display = 'none';
+            document.documentElement.style.overflow = 'auto';
+        }
+    })
+})
